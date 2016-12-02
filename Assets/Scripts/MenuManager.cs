f665c25883809c4c52ae4b3c07b9a7b1c7482892
Brian Flyte <bflyte90@gmail.com>
2016-12-02 11:51:38 -0500

    Add all Unity project files

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+    public void OnPlayClick()
+    {
+        SceneManager.LoadScene("MiniGame");
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text timerText;
+
+    private Rigidbody rb;
+    private int count;
+    private bool isTimerRunning;
+    private float realTime;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        Time.timeScale = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+            isTimerRunning = true;
+        }
+        if (isTimerRunning)
+        {
+            SetTimerText();
+            realTime += Time.deltaTime;
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            print("hello");
+            rb.MovePosition(new Vector3(0, 0, 0));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetTimerText()
+    {
+        timerText.text = realTime.ToString();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+            isTimerRunning = false;
+        }
+    }
+}
+
